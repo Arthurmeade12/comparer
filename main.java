b220781d1665e9eaa070a2d3e64f204d076457eb
Arthurmeade12 <arthurmeade12@gmail.com>
2024-02-02 17:03:45 -0600

    Add functional main

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
 package me.arthurmeade12.comparer;
+import java.util.Scanner;
 public class main {
   public static void main(String[] args){
     config.evalprops();
-    msg.out("Success!");
+    msg.out("Welcome to the comparer!");
+    gui.main();
+    msg.out_nonewline("Adjective ? : ");
+    Scanner scanner = new Scanner(System.in);
+    comparer exec = new comparer(scanner.next());
+    exec.print(exec.get_array());
+    msg.out("Bye!");
   }
 }
