dc661401d2240758683ba1ee74173ef978366191
Arthurmeade12 <arthurmeade12@gmail.com>
2024-02-02 17:02:29 -0600

    Add string utils

--- msg.java
+++ msg.java
@@ -1,5 +1,11 @@
 package me.arthurmeade12.comparer;
 public class msg {
+    public static String drop(String a, int b) {
+        return a.substring(0, a.length() - b);
+    }
+    public static String trim(String a, int b) {
+        return a.substring(a.length() - b);
+    }
     public static void out(String msg) {
         System.out.printf("\033[1;34m==>\033[0m \033[1;37m%s\033[0m%n", msg);
     }
